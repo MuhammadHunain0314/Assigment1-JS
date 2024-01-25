05ddd0c054a0a91345cd6e17b2eae7d2825ec014
MuhammadHunain0314 <156586251+MuhammadHunain0314@users.noreply.github.com>
2024-01-25 01:06:03 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+alert ("Hello User, Welcome to web page");
+
+let student1 = "Ahmed";
+let age = '17 years old';
+let isstudent = 'Yes, I am a student';
+
+console.log ( student1);
+console.log ( age);
+console.log ( isstudent);
